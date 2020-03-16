47f4572ea364850b3b1fc055e4e4a42790f331f0
Brain0ut <alexiy.v@gmail.com>
2020-03-16 15:23:38 +0200

    make some changes to fasion view ))

--- aradelka/Less_06_hwpr/num.py
+++ aradelka/Less_06_hwpr/num.py
@@ -1,17 +1,15 @@
 with open('numbers.txt', 'r') as file:
     for line in file:
         line1 = line.split(';')
-        # print(line[0], '+', line[1])
         a = line1[0].split()
-        b = line1[1].strip().split()
+        b = line1[1].split()
         k = 0
         for i in a:
             k = k + int(i)
 
         n = k // len(a)
         m = k % len(a)
-        #print(n, '+', m)
         if n == int(b[0]) and m == int(b[1]):
-            print(line+' True')
+            print(f"{' '.join(map(str, a))};{' '.join(map(str, b))} True")
         else:
-            print(line+' False')
+            print(f"{' '.join(map(str, a))};{' '.join(map(str, b))} False")
